9029fa33f7ec9fcfa7e55e71ebae28005e8e5d2b
LucasDiaszL <lucasjorge200247@gmail.com>
2026-03-02 23:43:18 -0300

    feat: seguranca de chaves com env e layout de cards padronizado

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -3,4 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
+// Este log vai te ajudar a testar (remova depois)
+console.log("URL do Supabase:", supabaseUrl); 
+
 export const supabase = createClient(supabaseUrl, supabaseKey)
