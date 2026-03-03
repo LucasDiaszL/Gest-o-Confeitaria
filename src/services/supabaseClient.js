c5d20932d29a9774589d547fc9fe85f97500ffa9
LucasDiaszL <lucasjorge200247@gmail.com>
2026-03-02 23:35:28 -0300

    feat: dark mode, controle de validade e layout dos cards padronizado

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://yahwicfbojloaezjtbkv.supabase.co'
-const supabaseKey = 'sb_publishable_G3udZDYZXeceyUgFdCzWBA_BfeST7_4'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
