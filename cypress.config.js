538de380010226d7e707414fda840253699e225d
Lucas dias <lucasjorge200247@gmail.com>
2026-06-15 16:31:27 -0300

    fix(qa): rename cypress config to .cjs to explicitly bypass ES Module type constraints

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://gest-o-confeitaria.vercel.app", // Garante a URL do seu deploy
+    baseUrl: "https://gest-o-confeitaria.vercel.app",
     setupNodeEvents(on, config) {
       // implemente node event listeners aqui se necessário
     },
-    // Proteção contra falhas de carregamento de recursos externos na esteira
     chromeWebSecurity: false, 
     video: false,
     screenshotOnRunFailure: false
