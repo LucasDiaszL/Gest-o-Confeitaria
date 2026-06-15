d87a0bb3a73fd80d431ae4c7b915f388e48118f0
Lucas dias <lucasjorge200247@gmail.com>
2026-06-15 16:26:38 -0300

    fix(qa): change cypress config syntax to commonjs to resolve node 22 module resolution

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from "cypress";
+const { defineConfig } = require("cypress");
 
-export default defineConfig({
-  allowCypressEnv: false,
-
+module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://gest-o-confeitaria.vercel.app", // Garante a URL do seu deploy
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implemente node event listeners aqui se necessário
     },
+    // Proteção contra falhas de carregamento de recursos externos na esteira
+    chromeWebSecurity: false, 
+    video: false,
+    screenshotOnRunFailure: false
   },
 });
